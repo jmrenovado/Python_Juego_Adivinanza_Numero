ceb535cfe3279ba61a52b2ce6da0543b78f89574
Jmrenovad0 <jhonmunoz411@gmail.com>
2024-12-04 15:49:20 -0500

    Juego de adivinanza de números desarrollado en Python

---
+++ juego-adivinanza-numero.py
@@ -0,0 +1,33 @@
+
+import random
+
+
+def juego_adivinanza():
+    numero_secreto = random.randint(1, 100)
+    intentos = 0
+    adivinado = False
+
+    print("¡Bienvenido al juego de adivinanza de números!")
+    print("Debes adivinar un número entre el 1 al 100")
+    print("¡Intenta adivinarlo!")
+
+    while not adivinado:
+        adivinanza = input("Introduzca un número del 1 al 100: ")
+
+        if adivinanza.isdigit():
+            adivinanza = int(adivinanza)
+            intentos += 1
+
+            if adivinanza < numero_secreto:
+                print(f"El número secreto es mayor a {adivinanza}")
+            elif adivinanza > numero_secreto:
+                print(f"El número secreto es menor a {adivinanza}")
+            else:
+                print(
+                    f"¡Felicitaciones has ganado! El número {adivinanza} es el secreto y lo has logrado en {intentos} intentos."
+                )
+        else:
+            print("Por favor introduzca un número válido entre el 1 al 100")
+
+
+juego_adivinanza()
